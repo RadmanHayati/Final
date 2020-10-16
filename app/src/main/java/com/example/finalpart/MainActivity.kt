7dfc27be379be5c3e0b1dc85f25cb9a6827b968e
radman <radman.hayatii@gmail.com>
2020-10-16 15:50:23 +0330

    begin

--- app/src/main/java/com/example/finalpart/MainActivity.kt
+++ app/src/main/java/com/example/finalpart/MainActivity.kt
@@ -8,5 +8,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val myVariable1=12*0
+        val myVariable2=12*0
+        val myVariable3=12*0
+        val myVariable4=12*0
+        val myVariable5=12*0
     }
 }
